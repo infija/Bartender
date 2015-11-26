50ae747ca49c6d261a9c3de5c8a869568fba2e06
Andrea Baldelomar <anbade@gmail.com>
2015-11-26 17:42:04 -0400

    add register form and change styles

--- aldehyde/content.php
+++ aldehyde/content.php
@@ -7,15 +7,25 @@
 <script type="text/javascript">
 jQuery(document).ready(function() {
 	//jQuery('.article-rest .entry-content').hide();
-        jQuery('.arrow-article-top').click(function(){
-            jQuery('.article-rest .entry-content').hide();
-            jQuery('.arrow-article-top').hide();
-            jQuery('.arrow-article-bottom').show();
+        jQuery('.arrows-article').click(function(event){
+            var id = event.currentTarget.id;
+            if(jQuery('#'+id+' .arrow-article-top').css('display')==="none"){
+                jQuery('#post-'+id+' .article-rest .entry-content').show();
+                jQuery('#'+id+' .arrow-article-bottom').hide();
+                jQuery('#'+id+' .arrow-article-top').show();
+            }else{
+                jQuery('#post-'+id+' .article-rest .entry-content').hide();
+                jQuery('#'+id+' .arrow-article-top').hide();
+                jQuery('#'+id+' .arrow-article-bottom').show();
+            }
         });
-        jQuery('.arrow-article-bottom').click(function(){
-            jQuery('.article-rest .entry-content').show();
-            jQuery('.arrow-article-bottom').hide();
-            jQuery('.arrow-article-top').show();
+        jQuery('.button-reserve').click(function(event){
+            var id = event.currentTarget.id.split("-");
+            if(jQuery('#form-'+id[1]).css('display')==="none"){
+                jQuery('#form-'+id[1]).show();
+            }else{
+                jQuery('#form-'+id[1]).hide();
+            }
         });
 });
 </script>
@@ -23,30 +33,32 @@ jQuery(document).ready(function() {
 <article id="post-<?php the_ID(); ?>" <?php post_class("row archive"); ?>>
 	
 	<div class="featured-thumb col-md-12 col-xs-12">
-	<div class="img-meta">
-		<div class="img-meta-link meta-icon"><a class='meta-link' href="<?php the_permalink() ?>"><i class="icon-link"></i></a></div>
-		<?php if (has_post_thumbnail()) : 
-					$thumb_id = get_post_thumbnail_id();
-					$thumb_url = wp_get_attachment_image_src($thumb_id,'full', true);
-		?>
-		<div class="img-meta-img meta-icon"><a class='meta-link meta-link-img' title="<?php the_title(); ?>" href="<?php echo $thumb_url[0] ?>"><i class="icon-picture"></i></a></div>
-		<?php endif; ?>
-	</div>
-	<a href="<?php the_permalink(); ?>">
-	<?php if (has_post_thumbnail()) :
-		the_post_thumbnail('homepage-banner');	
-	endif; 
-	?>
-	</a>
-	</div>
+            <div class="img-meta">
+                    <div class="img-meta-link meta-icon"><a class='meta-link' href="<?php the_permalink() ?>"><i class="icon-link"></i></a></div>
+                    <?php if (has_post_thumbnail()) : 
+                                            $thumb_id = get_post_thumbnail_id();
+                                            $thumb_url = wp_get_attachment_image_src($thumb_id,'full', true);
+                    ?>
+                    <div class="img-meta-img meta-icon"><a class='meta-link meta-link-img' title="<?php the_title(); ?>" href="<?php echo $thumb_url[0] ?>"><i class="icon-picture"></i></a></div>
+                    <?php endif; ?>
+                    </div>
+                    <a href="<?php the_permalink(); ?>">
+                    <?php if (has_post_thumbnail()) :
+                            the_post_thumbnail('homepage-banner');	
+                    endif; 
+                    ?>
+                    </a>
+            </div>
 	<div class="article-rest col-md-12">
 	<header class="entry-header">
-		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-
+		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+                </h1>
+                  
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php aldehyde_posted_on(); ?>
-		</div><!-- .entry-meta -->
+                        <div id="btn-<?php the_ID(); ?>" class="btn btn-success button-reserve">Reservar Cupo</div>  
+                </div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
@@ -69,7 +81,18 @@ jQuery(document).ready(function() {
 		?>
 	</div><!-- .entry-content -->
 	<?php endif; ?>
-        <div class="arrow-article-top"></div>
-        <div class="arrow-article-bottom"></div>
+        <div id="<?php the_ID(); ?>" class="arrows-article">
+            <div class="arrow-article-top"></div>
+            <div class="arrow-article-bottom"></div>
+        </div>
+        <div id="form-<?php the_ID(); ?>" class="reserve-form">
+            <form>
+                <input type="text" placeholder="Name" required/>
+                <input type="text" placeholder="Last name" required/>
+                <input type="number" placeholder="CI" required/>
+                <input type="submit" class="btn" value="Reservar"/>
+            </form>
+        </div>
 	</div>
+    
 </article><!-- #post-## -->
