71dc154a19fa5d015db6aea89bec3dc300c59ea3
Andrea Baldelomar <anbade@gmail.com>
2015-12-22 11:31:29 -0400

    add footer copyright

--- aldehyde/footer.php
+++ aldehyde/footer.php
@@ -11,19 +11,20 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer row" role="contentinfo">
-	<div class="container">
-	<?php if ( of_get_option('credit1', true) == 0 ) { ?>
-		<div class="site-info col-md-4">
-			<?php do_action( 'aldehyde_credits' ); ?>
-			<?php printf( __( 'Aldehyde Theme by %1$s.', 'aldehyde' ), '<a href="http://inkhive.com/product/aldehyde" rel="designer">InkHive</a>' ); ?>
-		</div><!-- .site-info -->
-	<?php } ?>	
-		<div id="footertext" class="col-md-7">
-        	<?php
-			if ( (function_exists( 'of_get_option' ) && (of_get_option('footertext2', true) != 1) ) ) {
-			 	echo of_get_option('footertext2', true); } ?>
-        </div>
-	</div>   
+            <div class="container">
+                        <div class="site-info col-md-4">
+                            <?php
+                                if ( (function_exists( 'of_get_option' ) && (of_get_option('footertext2', true) != 1) ) ) {
+                                       echo of_get_option('footertext2', true); } ?>
+                            <?php printf('<p>Copyright &copy Infija - 2015</p>'); ?>
+                        </div>    
+                    <?php if ( of_get_option('credit1', true) == 0 ) { ?>
+                        <div id="footertext" class="col-md-7">
+                                <?php do_action( 'aldehyde_credits' ); ?>
+                                <?php printf( __( 'Aldehyde Theme by %1$s.', 'aldehyde' ), '<a href="http://inkhive.com/product/aldehyde" rel="designer">InkHive</a>' ); ?>
+                        </div><!-- .site-info -->
+                <?php } ?>	
+            </div>   
 	</footer><!-- #colophon -->
 	
 </div><!-- #page -->
